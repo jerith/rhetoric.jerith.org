6d1eea426f289184b83752e874213d67a29b601d
Jeremy Thurgood <firxen@gmail.com>
2014-07-12 12:30:28 +0200

    Switch to pelican-elegant theme.

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Jeremy Thurgood'
 SITENAME = 'jerith dot org'
-SITEURL = ''
+SITEURL = 'http://localhost:8000'  # Overridden in publishconf.py
 SITESUBTITLE = 'Making the world a better place, one line of code at a time.'
 
 PATH = 'content'
@@ -33,8 +33,6 @@ TRANSLATION_FEED_ATOM = None
 # Social widget
 SOCIAL = (
     ('github', 'https://github.com/jerith'),
-    # ('You can add links in your config file', '#'),
-    # ('Another social link', '#'),
 )
 
 DEFAULT_PAGINATION = 10
@@ -42,11 +40,33 @@ DEFAULT_PAGINATION = 10
 ARTICLE_URL = "posts/{date:%Y}/{date:%m}/{slug}/"
 ARTICLE_SAVE_AS = ARTICLE_URL + "index.html"
 
-YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
-MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/index.html'
+# YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+# MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/index.html'
 
 DEFAULT_DATE_FORMAT = "%Y-%m-%d"
 
-# THEME = "themes/tuxlite_tbs"
-# THEME = "themes/tuxlite_zf"
-THEME = "themes/tuxlite_jerith"
+THEME = "themes/pelican-elegant"
+
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = [
+    'summary',
+    'sitemap',
+    'extract_toc',
+    'tipue_search',
+]
+MD_EXTENSIONS = [
+    'codehilite(css_class=highlight)',
+    'extra',
+    'headerid',
+    'toc',
+]
+DIRECT_TEMPLATES = (
+    ('index', 'tags', 'categories', 'archives', 'search', '404'))
+TAG_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+
+SITEMAP = {'format': 'xml'}
+
+RECENT_ARTICLE_SUMMARY = True
+SUMMARY_MAX_LENGTH = 100
